059ea774620faa3ed4c695afec900a83529709bd
Gusabavo <gustavo.sk.caxias@gmail.com>
2024-05-06 16:57:49 -0300

    Adicionar arquivos de projeto.

---
+++ ReferencesTesting/Elephant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ReferencesTesting
+{
+    internal class Elephant
+    {
+        public int EarSize;
+        public string Name;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("My name is: " + Name + ".");
+            Console.WriteLine("My ears are: " + EarSize + " inches tall.");
+        }
+    }
+}
